756e21c595c85abe3f26ab0d6d78bf1362210c54
Pierre K <pierrekn@gmail.com>
2022-01-15 09:23:53 +0100

    Refactoring: prepare for supporting different CashAddress prefixes  (#1924)
    * make CashAddress a base class and use a new BitcoinCashAddress subclass
    * rename local variable hrp to decodeHRP
    * make hrp a member of CashAddress and specify it in the constructors
    * add a few tests for Bitcoin Cash addresses
    * add "bitcoincash" as a class static variable, use HRP_BITCOINCASH

--- Package.swift
+++ Package.swift
@@ -12,13 +12,16 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/2.6.36/WalletCore.xcframework.zip",
-            checksum: "f8bad789644651f7153e5cf46bb3509fb457a14d35301f97863677235c2e602c"
+            url: "https://github.com/trustwallet/wallet-core/releases/download/2.7.2/WalletCore.xcframework.zip",
+            checksum: "391d8ddb3232248510bf69116a03927bef6f9520758226c4a6878117709a0251"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/2.6.36/SwiftProtobuf.xcframework.zip",
-            checksum: "40518a795e841bbd2c7e1f7019357b8e09b321a9c1ea264b0819f85575ac04c2"
+            url: "https://github.com/trustwallet/wallet-core/releases/download/2.7.2/SwiftProtobuf.xcframework.zip",
+            checksum: "4fbeaa7c07638eb758cec10adeb3cc33b063cdb92f35d4c1325beb46c4d124e7"
         )
     ]
 )
+
+
+
