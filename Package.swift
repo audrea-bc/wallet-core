55dcbbc518de816f22a13da90cc4351730139e13
Augustin Udrea <audrea@blockchain.com>
2022-03-31 19:42:39 +0200

    updated

--- Package.swift
+++ Package.swift
@@ -8,17 +8,17 @@ let package = Package(
         .library(name: "WalletCore", targets: ["WalletCore"]),
         .library(name: "SwiftProtobuf", targets: ["SwiftProtobuf"])
     ],
-    dependencies: [],
+    dependencies: [
+ .package(
+      name: "SwiftProtobuf",
+      url: "https://github.com/apple/swift-protobuf.git",
+      "1.15.0" ..< "2.0.0"
+    )],
     targets: [
         .binaryTarget(
             name: "WalletCore",
             url: "https://github.com/trustwallet/wallet-core/releases/download/2.7.2/WalletCore.xcframework.zip",
             checksum: "391d8ddb3232248510bf69116a03927bef6f9520758226c4a6878117709a0251"
-        ),
-        .binaryTarget(
-            name: "SwiftProtobuf",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/2.7.2/SwiftProtobuf.xcframework.zip",
-            checksum: "4fbeaa7c07638eb758cec10adeb3cc33b063cdb92f35d4c1325beb46c4d124e7"
         )
     ]
 )
