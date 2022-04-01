5aadfd79ac11c72fe8f777d88fff9276ad3b2219
Augustin Udrea <audrea@blockchain.com>
2022-04-01 13:11:24 +0200

    update

--- Package.swift
+++ Package.swift
@@ -17,10 +17,10 @@ let package = Package(
             url: "https://github.com/trustwallet/wallet-core/releases/download/2.7.6/WalletCore.xcframework.zip",
             checksum: "cc3e8691894bb416b8e484af5c26d0948e74d8eaa734817a5f39002f75d4a36b"
         ),
-        .binaryTarget(
-            name: "SwiftProtobuf",
-            url: "https://github.com/trustwallet/wallet-core/releases/download/2.7.6/SwiftProtobuf.xcframework.zip",
-            checksum: "7494c787da7e599ecd0166f9eaf7b7edfbba9c9669a6042be4abecbfd7e1ddc7"
-        )
+        // .binaryTarget(
+        //     name: "SwiftProtobuf",
+        //     url: "https://github.com/trustwallet/wallet-core/releases/download/2.7.6/SwiftProtobuf.xcframework.zip",
+        //     checksum: "7494c787da7e599ecd0166f9eaf7b7edfbba9c9669a6042be4abecbfd7e1ddc7"
+        // )
     ]
 )
