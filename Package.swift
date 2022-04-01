5bb698442512992ac96b25b69b49f6cec243b720
Augustin Udrea <audrea@blockchain.com>
2022-04-01 16:54:08 +0200

    removeProtobuf

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         .library(name: "WalletCore", targets: ["WalletCore"]),
-        .library(name: "SwiftProtobuf", targets: ["SwiftProtobuf"])
+        // .library(name: "SwiftProtobuf", targets: ["SwiftProtobuf"])
     ],
     dependencies: [],
     targets: [
@@ -16,10 +16,10 @@ let package = Package(
             url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.36-blockchain/WalletCore.xcframework.zip",
             checksum: "e6c1b2bfe08923f4cfa67fe7e5e43203fe8b47fc94325948eea5c93a41130469"
         ),
-        .binaryTarget(
-            name: "SwiftProtobuf",
-            url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.36-blockchain/SwiftProtobuf.xcframework.zip",
-            checksum: "b7f96148bba3639fe79a13f5e8905cf2e3d306de7cc3a9cf805eefaaaf2bc8ae"
-        )
+        // .binaryTarget(
+        //     name: "SwiftProtobuf",
+        //     url: "https://github.com/oliveratkinson-bc/wallet-core/releases/download/2.6.36-blockchain/SwiftProtobuf.xcframework.zip",
+        //     checksum: "b7f96148bba3639fe79a13f5e8905cf2e3d306de7cc3a9cf805eefaaaf2bc8ae"
+        // )
     ]
 )
